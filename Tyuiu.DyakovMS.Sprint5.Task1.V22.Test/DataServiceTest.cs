45e7855e98b41565199483d1d93ef7d91d7f5121
dyakovmaksim <pochta3478@mail.ru>
2024-12-10 00:19:09 +0500

    Task1

--- Tyuiu.DyakovMS.Sprint5.Task1.V22.Test/DataServiceTest.cs
+++ Tyuiu.DyakovMS.Sprint5.Task1.V22.Test/DataServiceTest.cs
@@ -18,6 +18,18 @@ namespace Tyuiu.DyakovMS.Sprint5.Task1.V22.Test
 
             Assert.IsTrue(File.Exists(filePath));
             Assert.AreEqual("x\tF(x)", resultLines[0]);
+
+            string[] expectedResults = new[]
+           {
+                "-8.86", "-7.19", "-6.14", "-4.76", "-2.33", "1.00",
+                "4.38", "0.00", "6.13", "7.07", "8.61"
+            };
+
+            for (int i = 1; i <= 11; i++) // От -5 до 5
+            {
+                string[] parts = resultLines[i].Split('\t');
+                Assert.AreEqual(expectedResults[i - 1], parts[1]);
+            }
         }
     }
 }
