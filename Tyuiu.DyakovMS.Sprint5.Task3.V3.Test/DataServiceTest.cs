24e020ce66443ed774e444f67c145f2cd9a1ef8e
dyakovmaksim <pochta3478@mail.ru>
2024-12-13 20:37:06 +0500

    Task3

---
+++ Tyuiu.DyakovMS.Sprint5.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DyakovMS.Sprint5.Task3.V3.Lib;
+
+namespace Tyuiu.DyakovMS.Sprint5.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            string filePath = ds.SaveToFileTextData(x);
+            Assert.IsTrue(File.Exists(filePath));
+        }
+    }
+}
---
+++ Tyuiu.DyakovMS.Sprint5.Task3.V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.DyakovMS.Sprint5.Task3.V3.Lib;
+namespace Tyuiu.DyakovMS.Sprint5.Task3.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Дьяков Максим Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* потоковый метод записи данных в бинарный файл                           *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                            *");
+            Console.WriteLine("* Выполнил: Дьяков Максим Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дано выражение вычислить его значение при x = 3, результат сохранить в би*");
+            Console.WriteLine("*инарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до трёх *");
+            Console.WriteLine("* знаков после запятой.                                                   *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int x = 3;
+            Console.WriteLine($"Вычисление выражения y(x) = x * sqrt(x + 3) при x = {x}");
+            string filePath = ds.SaveToFileTextData(x);
+            double result = ds.ReadFromBinaryFile(filePath);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Результат: {result}");
+            Console.WriteLine($"Результат сохранен в файл: {filePath}");
+            Console.ReadKey();
+        }
+    }
+}
